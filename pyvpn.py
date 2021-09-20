0b3ec6b64a8d2070f4fe8a5e3a0a152d2ce8f5b3
Pinto565 <pintoinfant5650@gmail.com>
2021-09-20 17:19:36 +0530

    ip in response

--- pyvpn.py
+++ pyvpn.py
@@ -1,13 +1,14 @@
 import subprocess
 import smtplib
-from ip_addr import generate_tun_ip
+from ip_addr import *
 from email.message import EmailMessage
 
 
-def cert_json(name , email , status):
+def cert_json(name , email , ip,status):
     op = {
         "name" : name , 
-        "email" : email , 
+        "email" : email ,
+        "ip_adress" : ip,
         "status" : status
     }
     return op
@@ -48,10 +49,10 @@ def gen_cert(name , email):
         process = subprocess.run(f"echo \"ifconfig-push {tun_ip} 255.255.255.0\" > /etc/openvpn/ccd/{lower_name}",shell=True)
         process = subprocess.run(f"openssl x509 -subject -noout -in /etc/openvpn/easy-rsa/pki/issued/{lower_name}.crt",shell=True)
         send_mail(name,email)
-        op = cert_json(name , email , True)
+        op = cert_json(name , email ,tun_ip, True)
         return op
     except:
-        op = cert_json(name , email , False)
+        op = cert_json(name , email ,tun_ip, False)
         return op
 
 def bad_request(reason):
