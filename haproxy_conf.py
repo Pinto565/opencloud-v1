1962e144447eea7563c47de52a5111ea09f8e5f3
Pinto565 <pintoinfant5650@gmail.com>
2021-09-22 20:29:13 +0530

    seprated tcp and http

--- haproxy_conf.py
+++ haproxy_conf.py
@@ -3,18 +3,30 @@ import random , string
 conf_file = "/etc/haproxy/haproxy.cfg"
 
 def random_code():
-    characters = string.ascii_letters
+    characters = string.ascii_lowercase
     code = ''.join(random.choice(characters) for i in range(8))
     return code
 
-def write_conf(port,device,dev_port):
+def write_http_conf(device,dev_port):
   file = open(conf_file, "a")
   id = random_code()
   # file.truncate(0)
-  file.write(f"\n\nfrontend front_{id}")
-  file.write(f"\n   bind {id}.node.opencloud.world:{port}")
-  file.write(f"\n   default_backend back_{id}")
-  file.write(f"\nbackend back_{id}")
-  file.write(f"\n   balance      roundrobin")
-  file.write(f"\n   server {id}      {device}:{dev_port} check\n")
+  public_add = f"{id}.node.opencloud.world"
+  file.write(f"\nbackend {public_add}")
+  file.write(f"\n   server {id}      {device}:{dev_port}\n")
   file.close()
+  return public_add
+
+def write_ssh_conf(device,dev_port):
+  file = open(conf_file, "a")
+  id = random_code()
+  # file.truncate(0)
+  public_add = f"{id}.node.opencloud.world"
+  file.write(f"\n\nfrontend front_{id}")
+  file.write(f"\n   mode tcp")
+  file.write(f"\n   bind {public_add}:8022")
+  file.write(f"\n   use_backend back_{id}")
+  file.write(f"\nbackend back_{id}")
+  file.write(f"\n   server {id}      {device}:{dev_port}\n")
+  file.close()
+  return public_add
