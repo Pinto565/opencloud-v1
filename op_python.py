f989c34c25fa5320a7c1d350b54f529f6fa0fa4b
Pinto565 <pintoinfant5650@gmail.com>
2021-10-09 23:12:11 +0530

    Check for valid git url

--- op_python.py
+++ op_python.py
@@ -1,6 +1,7 @@
 import subprocess
 import random
 import sys
+import requests
 
 cmd = subprocess.run("pwd",capture_output=True,text=True,shell=True)
 directory = cmd.stdout.strip()
@@ -8,44 +9,48 @@ directory = f"{directory}/deployments"
 cmd = subprocess.run(f"mkdir {directory}",capture_output=True,text=True,shell=True)
 print("Directory Created")
 git_url = sys.argv[-1]  #input("Enter the Git URL > ")
-if ".git" in git_url:
-    print("Cloning the Github Repository")
-    try:
-        cmd = subprocess.run(f"cd {directory} && git clone {git_url}",capture_output=True,text=True,shell=True)
-        print("Repository Cloned")
-        words = git_url.split('/')
-        folder = (words[len(words)-1].replace(".git",""))
-        git_directory = (f"{directory}/{folder}")
-        print("Creating Virtual Python Environment")
+status = requests.get(git_url).status_code
+if status == "200":
+    if ".git" in git_url:
+        print("Cloning the Github Repository")
         try:
-            cmd = subprocess.run(f"cd {git_directory} && python3 -m venv venv",capture_output=True,text=True,shell=True)
-            print("Virtual Environment Created..")
-            print("Activating Virtual Environment..")
+            cmd = subprocess.run(f"cd {directory} && git clone {git_url}",capture_output=True,text=True,shell=True)
+            print("Repository Cloned")
+            words = git_url.split('/')
+            folder = (words[len(words)-1].replace(".git",""))
+            git_directory = (f"{directory}/{folder}")
+            print("Creating Virtual Python Environment")
             try:
-                cmd = subprocess.run(f"cd {git_directory} && source venv/bin/activate",capture_output=True,text=True,shell=True)
-                print("Virtual Environment Activated...")
-                print("Installing Dependencies..")
+                cmd = subprocess.run(f"cd {git_directory} && python3 -m venv venv",capture_output=True,text=True,shell=True)
+                print("Virtual Environment Created..")
+                print("Activating Virtual Environment..")
                 try:
-                    cmd = subprocess.run(f"cd {git_directory} && {git_directory}/venv/bin/pip3 install -r {git_directory}/requirements.txt",capture_output=True,text=True,shell=True)
+                    cmd = subprocess.run(f"cd {git_directory} && source venv/bin/activate",capture_output=True,text=True,shell=True)
+                    print("Virtual Environment Activated...")
+                    print("Installing Dependencies..")
                     try:
-                        with open(f"{git_directory}/entrypoint") as file:
-                            list = file.read().split()
-                            if list[0] == "gunicorn":
-                                arg1 = list[0].replace(list[0],f"{git_directory}/venv/bin/{list[0]}")
-                                arg2 = list[-1]
-                                entrypoint = (f"{arg1} {arg2} --bind 0.0.0.0")
-                                entrypoint = (f"cd {git_directory} && {entrypoint} --daemon")
-                                print("Your Application is Running on 0.0.0.0:8000")
-                                cmd = subprocess.run(entrypoint,shell=True)
+                        cmd = subprocess.run(f"cd {git_directory} && {git_directory}/venv/bin/pip3 install -r {git_directory}/requirements.txt",capture_output=True,text=True,shell=True)
+                        try:
+                            with open(f"{git_directory}/entrypoint") as file:
+                                list = file.read().split()
+                                if list[0] == "gunicorn":
+                                    arg1 = list[0].replace(list[0],f"{git_directory}/venv/bin/{list[0]}")
+                                    arg2 = list[-1]
+                                    entrypoint = (f"{arg1} {arg2} --bind 0.0.0.0")
+                                    entrypoint = (f"cd {git_directory} && {entrypoint} --daemon")
+                                    print("Your Application is Running on 0.0.0.0:8000")
+                                    cmd = subprocess.run(entrypoint,shell=True)
+                        except:
+                            print("Application Failed..")
                     except:
-                        print("Application Failed..")
+                        print("Installation Failed..")
                 except:
-                    print("Installation Failed..")
+                    print("Activation of Virtual Environment Failed..")
             except:
-                print("Activation of Virtual Environment Failed..")
+                print("Virtual Environment Failed")
         except:
-            print("Virtual Environment Failed")
-    except:
-        print("Repository Cloning Failed")
+            print("Repository Cloning Failed")
+    else:
+        print("Please Enter a Git URL")
 else:
-    print("Please Enter a Git URL")
+    print("Provide a Valid Public Repository")
